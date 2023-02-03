7887507f8669971b5c73ac65a76fb062b027efca
Ola Fawzi <olafawzii80@gmail.com>
2023-02-04 01:41:19 +0200

    add docs to hosting

---
+++ docs/assets/js/me.js
@@ -0,0 +1,20 @@
+
+// function d ()
+// {
+// let x = document.querySelector( ".back-img" );
+// let y = document.querySelector( ".login" );
+
+// console.log( x );
+//     if ( window.matchMedia( "(man-width: 1000px)" ).matches )
+// {
+//     console.log( "true" );
+
+//     x.classList.remove( "col-6" );
+//     // y.classList.replace( "col-6", "col-12" );
+// } else
+// {
+//     x.classList.add( "col-6" );
+// }
+// }
+// d();
+
